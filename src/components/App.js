8e047b67179b620b07a38b14ee6a884d2e688030
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-07-18 14:31:41 -0400

    ex4 done

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,7 @@ const App = (props) => {
           handleSelect={(suggestion) => {
           window.alert(suggestion)
           }}
+          categories={data.categories}
         />
     </>
   );
--- src/components/Typeahead.js
+++ src/components/Typeahead.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Typeahead= ({suggestions, handleSelect}) => {
+const Typeahead= ({suggestions, handleSelect, categories}) => {
   const [value, setValue] = React.useState('');
   const matchedSuggestions = suggestions.filter(suggestion =>suggestion.title.toLowerCase().includes(value.toLowerCase())
   );
-
+  const splitSuggestion = value.slice(0,value.length);
+  console.log(splitSuggestion);
   return (
       <Container>
       <Wrapper>
@@ -22,12 +23,17 @@ const Typeahead= ({suggestions, handleSelect}) => {
       <ClearButton onClick={() => setValue('')}>Clear</ClearButton>
       <SuggestionList>
           {value.length >= 2 && matchedSuggestions.map((suggestion) => {
+            //   console.log(suggestion.title.slice(0 ,suggestion.title.toLowerCase().indexOf(value)+ value.length + 1),suggestion.title.slice(suggestion.title.toLowerCase().indexOf(value) + value.length + 1));
               return (
                 <Suggestion 
                   key={suggestion.id}
                   onClick={() => handleSelect(suggestion.title)}
                 >
-                  {suggestion.title}
+                  <span>
+                    {suggestion.title.slice(0 ,suggestion.title.toLowerCase().indexOf(value)+ value.length)}
+                    <Prediction>{suggestion.title.slice(suggestion.title.toLowerCase().indexOf(value) + value.length)}</Prediction>
+                  </span>
+                   &nbsp;in <BookCategory>{suggestion.categoryId}</BookCategory>
                 </Suggestion>
               );
           })}
@@ -39,16 +45,16 @@ const Typeahead= ({suggestions, handleSelect}) => {
 
 const Container = styled.div`
   position: relative;
-`
+`;
 const Wrapper = styled.div`
   margin-top: 100px;
   position: absolute;
   left: 200px;
-`
+`;
 const BookInput = styled.input`
   padding: 10px 100px 10px 10px;
   text-align: left;
-`
+`;
 const ClearButton = styled.button`
   padding: 10px 15px;
   background-color: darkblue;
@@ -65,15 +71,22 @@ const SuggestionList = styled.ul`
       padding-bottom: 0px;
       border-bottom: none;
   }
-`
+`;
 const Suggestion = styled.li`
   text-decoration: none;
   padding-top: 3px;
   padding-bottom: 3px;
   border-bottom: 1px solid grey;
-
+  width: 355px;
   &:hover{
       background-color: #f7e890;
   }
+`;
+const BookCategory = styled.span`
+  color:purple;
+  font-style: italic;
+`;
+const Prediction = styled.span`
+  font-weight: bold;
 `
 export default Typeahead;
