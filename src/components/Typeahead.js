491ffd7af9ae779a05f0d07f415d7c526bda3efb
jenny ma <jenny_lau_8@hotmail.com>
2020-07-19 21:33:18 -0400

    E1 complete, css input and clear button

--- src/components/Typeahead.js
+++ src/components/Typeahead.js
@@ -1,12 +1,47 @@
 import React from 'react';
+import styled from 'styled-components';
 
 
 
 
-
-
-const Typeahead = (props) => {
-    return ("hello")
+const Typeahead = ({suggestions, categories, handleSelect}) => {
+    const [value, setValue] = React.useState('');
+    return (
+    <Wrapper>
+      <Input
+        type='text'
+        value={value}
+        onChange={(ev) => setValue(ev.target.value)}
+        onKeyDown={(ev) => {
+          if (ev.key === 'Enter') {
+            handleSelect(ev.target.value);
+          }
+        }}></Input>
+      <ClearButton onClick={() => setValue('')}>Clear
+      </ClearButton>
+    </Wrapper>
+    );
 };
+
+const Wrapper = styled.div`
+    display: flex;
+`;
+
+const Input = styled.input`
+    width: 350px;
+`;
+
+const ClearButton = styled.button`
+    text-decoration: none;
+    color: white;
+    margin-left: 10px;
+    background-color: darkblue;
+    border: none;
+    padding: 10px;
+    width: 80px;
+    font-size: 15px;
+    border-radius: 5px;
+
+`;
 
 export default Typeahead;
