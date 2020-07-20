b8b41c1ca3d09f088d2ce9fe06217b1c4b7387ad
Andres Rueda <garo99@gmail.com>
2020-07-20 15:15:56 -0400

    checking variables

--- src/components/MatchFormatter.js
+++ src/components/MatchFormatter.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
+import { categories } from '../data';
 
 const MatchFormatter = ({ book, string = '' }) => {
   const index = book.title.toLowerCase().indexOf(string);
   const firstHalf = book.title.slice(0, index - 1);
   const secondHalf = book.title.slice(index - 1);
-  console.log(book.categoryId);
-
+  let textCategory = '';
+  if (book.categoryId !== undefined) {
+    // textCategory = ' as ' + categories[book.categoryId].name;
+    console.log(categories[book.categoryId].name);
+  }
   return (
     <>
       <span>{firstHalf}</span>
@@ -20,7 +24,9 @@ const Bold = styled.span`
   font-style: italic;
   &::after {
     font-weight: 400;
-    content: " as ${(book) => book.category}";
   }
 `;
 export default MatchFormatter;
+
+// content: " as ${(book) => book.category}";
+// content: "${textCategory}"
