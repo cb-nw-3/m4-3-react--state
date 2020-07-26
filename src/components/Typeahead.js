c249d817f2879c9c3919146d9fabdd610533e81d
WilfredoJoseph Hiwatig <jhiwatig.dev@gmail.com>
2020-07-26 16:33:20 -0400

    Finished Excercise 2

--- src/components/Typeahead.js
+++ src/components/Typeahead.js
@@ -25,16 +25,44 @@ const Clear = styled.button`
   cursor: pointer;
 `;
 
-const Typeahead = ({ data, handleSelect }) => {
+const Wrapper = styled.div`
+  position: relative;
+`;
+
+const Suggestions = styled.ul`
+  list-style-type: none;
+  border-radius: 4px;
+  margin-top: 5px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  padding: 10px;
+`;
+
+const ListItem = styled.li`
+  padding: 10px;
+  margin: 10px;
+  max-width: 350px;
+  cursor: pointer;
+  &:hover {
+    background-color: #fff9cf;
+  }
+`;
+
+const Typeahead = ({ suggestions, handleSelect }) => {
   const [value, setValue] = React.useState("");
+  const [showSuggestions, setShowSuggestions] = React.useState(false);
 
   return (
     <Container>
-      <div>
+      <Wrapper>
         <Input
           type="text"
           onChange={(ev) => {
             setValue(ev.target.value);
+            if (value.length > 1) {
+              setShowSuggestions(true);
+            } else {
+              setShowSuggestions(false);
+            }
           }}
           onKeyDown={(ev) => {
             if (ev.key === "Enter") {
@@ -49,12 +77,33 @@ const Typeahead = ({ data, handleSelect }) => {
             const input = document.querySelector("input");
             input.value = "";
             setValue("");
+            setShowSuggestions(false);
           }}
         >
           Clear
         </Clear>
-        <h1>{value}</h1>
-      </div>
+        {showSuggestions && (
+          <Suggestions>
+            {suggestions.map((book) => {
+              const match = book.title
+                .toLowerCase()
+                .includes(value.toLowerCase());
+              if (match) {
+                return (
+                  <ListItem
+                    key={book.id}
+                    onClick={(ev) => {
+                      handleSelect(book.id);
+                    }}
+                  >
+                    {book.title}
+                  </ListItem>
+                );
+              }
+            })}
+          </Suggestions>
+        )}
+      </Wrapper>
     </Container>
   );
 };
