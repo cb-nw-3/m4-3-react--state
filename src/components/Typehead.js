77c6114ef5900f0a7dbf7ad890e48c9322916e45
Alex Langevin <alex_langevin@hotmail.ca>
2020-08-01 11:16:46 -0400

    Exercice 2 Completed with a minor bug

--- src/components/Typehead.js
+++ src/components/Typehead.js
@@ -4,6 +4,16 @@ import styled from 'styled-components';
 function Typehead ({suggestions, handleSelect}) {
     const [value, setValue] = React.useState('');
 
+    function guessBooks (input){
+        if (input.length > 1) {
+            return suggestions.filter(book => book.id.toLowerCase().includes(input.toLowerCase()));
+        } else {
+            return [];
+        }
+    };
+    const bookSuggestions = guessBooks(value);
+    console.log(bookSuggestions);
+
     return (
         <Wrapper>
             <SearchInput
@@ -17,6 +27,15 @@ function Typehead ({suggestions, handleSelect}) {
                 }}
             />
             <ClearButton onClick={() => setValue('')}>Clear</ClearButton>
+            <Break />
+            <SuggestionList>
+                {
+                bookSuggestions.map((book) => {
+                    return(
+                        <Suggestion>{book.id}</Suggestion>
+                    ); 
+                })}
+            </SuggestionList>
         </Wrapper>
     );
 }
@@ -24,15 +43,17 @@ function Typehead ({suggestions, handleSelect}) {
 const Wrapper = styled.div`
     margin: 100px auto 0 auto;
     height: 40px;
+    width: 750px;
     display: flex;
-    justify-content: center;
+    justify-content: flex-start;
+    flex-wrap: wrap;
 `;
 
 const SearchInput = styled.input`
     border:solid 2px rgb(66, 135, 245);
     border-radius: 3px;
     height: 100%;
-    width: 375px;
+    flex-basis:80%;
     margin-right: 10px;
     font-size: 18px;
 `;
@@ -43,8 +64,26 @@ const ClearButton = styled.button`
     background-color: blue;
     color: white;
     height: 100%;
-    width: 75px;
+    flex-basis: 15%;
     font-size: 18px;
 `;
+
+const SuggestionList =  styled.ul`
+    margin-right:28px;
+    flex-basis:100%;
+    font-size: 18px;
+    padding: 10px;
+    box-shadow: 0px 2px 10px rgba(0,0,0,0.2);
+`
+
+const Suggestion = styled.li`
+    flex-basis:100%;
+    padding: 8px;
+`
+
+const Break = styled.div`
+    flex-basis: 100%;
+    height:0px;
+`
 
 export default Typehead;
